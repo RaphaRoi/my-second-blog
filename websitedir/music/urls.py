24c1f837b88f4b755981afc662518d9ad12004db
rapharoi <salimraphael@gmail.com>
2017-07-01 20:04:12 -0700

    add html templates

--- websitedir/music/urls.py
+++ websitedir/music/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /music/
     url(r'^$', views.index, name='index'),
-    url(r'about', views.about, name='about')
+
+    # /music/712/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- websitedir/music/views.py
+++ websitedir/music/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Album
 
 
 def index(request):
-    return HttpResponse("<h1>This is the Music app homepage</h1>")
+    all_albums = Album.objects.all()
+    context = {'all_albums': all_albums}
+    return render(request, 'music/index.html', context)
 
 
-def about(request):
-    return HttpResponse("This is the about page!!")
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for album id: " + str(album_id) + "</h2>")
+
